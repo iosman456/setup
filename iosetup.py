7f5ca9fe7300329aebe2594cec22f021e70fad34
iosman456 <muhammetcanaltan41@gmail.com>
2025-03-08 22:14:24 +0300

    Update iosetup.py

--- iosetup.py
+++ iosetup.py
@@ -2,18 +2,30 @@ import os
 import subprocess
 import sys
 
+def is_root():
+    return os.geteuid() == 0
+
+def check_package_installed(package_name):
+    try:
+        result = subprocess.run(["apk", "info", package_name], check=True, capture_output=True, text=True)
+        if package_name in result.stdout:
+            return True
+        return False
+    except subprocess.CalledProcessError:
+        return False
+
 def install_package(package_name):
+    if check_package_installed(package_name):
+        print(f"{package_name} zaten yüklü.")
+        return
     try:
         result = subprocess.run(["apk", "add", package_name], check=True, capture_output=True, text=True)
         print(f"{package_name} başarıyla yüklendi!\n{result.stdout}")
     except subprocess.CalledProcessError as e:
-        if "is already installed" in e.stderr:
-            print(f"{package_name} zaten yüklü.")
-        else:
-            print(f"{package_name} yüklenirken hata oluştu:\n{e.stderr}")
+        print(f"{package_name} yüklenirken hata oluştu:\n{e.stderr}")
 
 def main():
-    if os.geteuid() != 0:
+    if not is_root():
         print("Bu scripti çalıştırmak için root yetkisine sahip olmalısınız.")
         sys.exit(1)
 
