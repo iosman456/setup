bf1eca8971b55e0e1610bf59aee8a059f0a73e4b
iosman456 <muhammetcanaltan41@gmail.com>
2025-03-08 21:57:55 +0300

    Update and rename setup.py to iosetup.py

---
+++ iosetup.py
@@ -0,0 +1,10 @@
+import os
+
+print("İOS'da Gereken Paketler Yükleniyor...")
+os.system("apk add update")
+os.system("apk add upgrade")
+os.system("apk add python2
+os.system("apk add python3
+os.system("apk add py-pip")
+os.system("apk add py3-pip")
+print("Gereken Paketler Yüklendi!")
